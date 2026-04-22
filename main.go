56ebafe3039ab5b4c5af03377bc45e3627e7ffdd
k.barylnikov <ipt_git_bot@ipt.ru>
2026-04-22 22:55:06 +0300

    2 hw

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
+import "fmt"
+
 
 func main() {
 	const usdToEur = 0.85
 	const usdToRub = 75.0
 	const eurToRub = usdToRub / usdToEur
 }
+
+func scanData() (string) {
+	var data string
+	fmt.Scan(&data)
+	return data
+}
+
+func convertCurrency(amount float64, from string, to string) {}
